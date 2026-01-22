df301372ba7a04763bf25d25e313fe892ad5c421
Khuram Mirza <mirzakhuram101@gmail.com>
2026-01-23 01:36:28 +0500

    feat(product/model): create product model

---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,40 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    sku: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export type Product = InferSchemaType<typeof ProductSchema>;
+export const ProductModel = model<Product>("Product", ProductSchema);
