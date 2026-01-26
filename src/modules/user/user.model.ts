91347113a52fe3f71de227b9765d97fc4a2846b2
Khuram Mirza <mirzakhuram101@gmail.com>
2026-01-27 00:47:31 +0500

    feat(user/model): create basic user model

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,34 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false, // Security: Never return password by default in queries
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Auto-generate TypeScript Type
+export type User = InferSchemaType<typeof UserSchema>;
+export const UserModel = model<User>("User", UserSchema);
