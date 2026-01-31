264a03bbe9c52fdfdbe65f0de4921d3c4b6d74f6
Khuram Mirza <mirzakhuram101@gmail.com>
2026-02-01 00:17:15 +0500

    feat(user/service): create updateMe, and updateMyPassword service functions

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,6 +4,52 @@ import { UserModel } from "./user.model.js";
 import { RegisterInput, LoginInput } from "./user.schema.js";
 import { AppError } from "../../common/utils/AppError.js";
 
+// Helper: Only allow specific fields to be updated
+export const filterObj = (obj: any, ...allowedFields: string[]) => {
+  const newObj: any = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+export const updateMyPassword = async (userId: string, filteredBody: any) => {
+  // 1. Find the User
+  const user = await UserModel.findById(userId).select("+password");
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
+  // 2. Check if Posted current password is correct
+  const isMatch = await user.matchPassword(filteredBody.passwordCurrent);
+
+  if (!isMatch) {
+    throw new AppError("Your current password is wrong.", 401);
+  }
+
+  // 3. If so, update password
+  user.password = await bcrypt.hash(filteredBody.passwordNew, 10);
+  await user.save();
+};
+
+export const updateMe = async (userId: string, filteredBody: any) => {
+  // 1. Create error if user POSTs password data here
+  if (filteredBody.password || filteredBody.passwordConfirm) {
+    throw new AppError(
+      "This route is not for password updates. Please use /updateMyPassword.",
+      400,
+    );
+  }
+
+  // 2. Update user document
+  const updatedUser = await UserModel.findByIdAndUpdate(userId, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  return updatedUser;
+};
+
 export const registerUser = async (data: RegisterInput) => {
   console.log("login");
   const existingUser = await UserModel.findOne({ email: data.email });
