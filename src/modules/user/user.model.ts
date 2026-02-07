2c11a9f1256010388f19061edcebd48126e769c4
Khuram Mirza <mirzakhuram101@gmail.com>
2026-02-07 23:23:29 +0500

    feat(user/model): create passwordResetToken and passwordResetExpires fields, and createPasswordResetToken method.

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,9 +1,11 @@
 import bcrypt from "bcryptjs";
+import crypto from "crypto";
 import { InferSchemaType, Schema, Model, model } from "mongoose";
 
 // 1. Define the interface for your custom methods
 interface UserMethods {
   matchPassword(enteredPassword: string): Promise<boolean>;
+  createPasswordResetToken(): string;
 }
 
 const UserSchema = new Schema(
@@ -29,6 +31,8 @@ const UserSchema = new Schema(
       enum: ["user", "admin"],
       default: "user",
     },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
   {
     timestamps: true,
@@ -42,6 +46,23 @@ UserSchema.methods.matchPassword = async function (
   enteredPassword: string,
 ) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+UserSchema.methods.createPasswordResetToken = function () {
+  // A. Generate a random 32-byte hex string (The "Key")
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  // B. Hash it and save to DB (Security: Never save plain tokens!)
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // C. Set expiration (10 minutes from now)
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  // D. Return the PLAIN token (to send in the email)
+  return resetToken;
 };
 
 export const UserModel = model<User, Model<User, {}, UserMethods>>(
